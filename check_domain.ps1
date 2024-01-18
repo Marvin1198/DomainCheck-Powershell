6b3adf8c55c86880e68d61099d770f20ea542923
Marvin Patel <marvinpatel42@gmail.com>
2024-01-17 20:48:51 -0500

    Add files via upload

---
+++ check_domain.ps1
@@ -0,0 +1,16 @@
+﻿# Check if the computer is in a domain
+$domainInfo = Get-WmiObject -Class Win32_ComputerSystem
+if ($domainInfo.PartOfDomain -eq $true) {
+    Write-Host "The computer is a member of a domain."
+
+    # Check if the computer is a domain controller
+    $domainController = Get-ADDomainController -Discover
+    if ($domainController) {
+        Write-Host "The computer is a domain controller."
+        Start-Sleep -Seconds 3
+    } else {
+        Write-Host "The computer is not a domain controller."
+    }
+} else {
+    Write-Host "The computer is not a member of a domain."
+}
